662a1d9fa9d0259ad016f430a0b12241ad49045c
DLNY971237 <danny971237@gmail.com>
2024-06-04 15:32:20 +0800

    modify

--- 2024_06-04/index.py
+++ 2024_06-04/index.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 import tkinter as tk
 from tkinter import ttk
-from ttkthemes import ThemedTk # type: ignore
+from ttkthemes import ThemedTk
 import tools
 
 class Window(ThemedTk):
@@ -11,14 +11,28 @@ class Window(ThemedTk):
         #self.option_add("*Font","微軟正黑體 40")
         #定義style的名稱
         style = ttk.Style()
-        style.configure('Top.TFrame',background='#BEC23F')
-        style.configure('Top.TLabel',font=('Helvetica',25))
+        style.configure('Top.TFrame')
+        style.configure('Top.TLabel',font=('Helvetica',25,'bold'))
 
         title_frame = ttk.Frame(self,style='Top.TFrame',borderwidth=2,relief='groove')
         ttk.Label(title_frame,text='全台空氣品質指標(AQI)',style='Top.TLabel').pack(expand=True,fill='y')
-        title_frame.pack(padx=100,pady=100,ipadx=100,ipady=25)
-        
+        title_frame.pack(ipadx=100,ipady=30,padx=10,pady=10)
 
+        func_frame = ttk.Frame(self,style='Top.TFrame',borderwidth=1,relief='groove')
+        ttk.Button(func_frame,text="AQI品質最好的5個",command=self.click1).pack(side='left',expand=True)
+        ttk.Button(func_frame,text="AQI品質最差的5個",command=self.click2).pack(side='left',expand=True)
+        ttk.Button(func_frame,text="pm2.5品質最好的5個",command=self.click3).pack(side='left',expand=True)
+        ttk.Button(func_frame,text="pm2.5品質最好的5個",command=self.click4).pack(side='left',expand=True)
+        func_frame.pack(ipadx=100,ipady=30,padx=10,pady=10)
+
+    def click1(self):
+        print("click1")
+    def click2(self):
+        print("click2")
+    def click3(self):
+        print("click3")
+    def click4(self):
+        print("click4")
 
 def main():
     '''
