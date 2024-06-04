525d92c09876dd0d07cc530c00268a873ca753be
DLNY971237 <danny971237@gmail.com>
2024-06-04 13:41:46 +0800

    modify

--- 2024_06-04/index.py
+++ 2024_06-04/index.py
@@ -1,15 +1,25 @@
 from pprint import pprint
+import tkinter as tk
+from tkinter import ttk
 import tools
 
+class Window(tk.Tk):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.title("AQI顯示")
 
 def main():
-    try:
+    '''
+     try:
         all_data:dict[any] = tools.download_json()
     except Exception as error:
         print(error)
     else:        
         data:list[dict] = tools.get_data(all_data)
-        pprint(data)
+        pprint(data)'
+    '''
+    window = Window()
+    window.mainloop()
 
 if __name__ == '__main__':
     main()
