29690f303e8ef395b8386c34d6befe551420bded
Danny Lin <danny971237@gmail.com>
2024-06-11 09:25:41 +0800

    modify

---
+++ 2024_06_11/0610作業.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from ttkthemes import ThemedTk
+
+class Window(ThemedTk):
+    def __init__(self.theme:str,**kwargs)
+
+
+def main():
+    window = Window(theme='arc')
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
