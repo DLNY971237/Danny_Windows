a8f5092cba1408b3e0a3fa4cf18d8ef66218d726
Danny Lin <danny971237@gmail.com>
2024-06-11 15:12:37 +0800

    modify

--- 2024_06_11/tools.py
+++ 2024_06_11/tools.py
@@ -50,3 +50,15 @@ class CustomMessagebox(Dialog):
         self.parent.name_value.set('')
         self.parent.hight_value.set('')
         self.parent.weight_value.set('')
+
+    def buttonbox(self):
+        # Add custom buttons (overriding the default buttonbox)
+        box = ttk.Frame(self)
+        self.ok_button = tk.Button(box, text="確定", width=10, command=self.ok, default=tk.ACTIVE)
+        self.ok_button.pack(side=tk.LEFT, padx=5, pady=5)
+        box.pack()
+
+    def ok(self):
+        # Override the ok method
+        print("OK button was clicked!")
+        super().ok()
