be9bd6c691ddb06002194f4984811045ecc71ba2
Danny Lin <danny971237@gmail.com>
2024-06-17 09:17:28 +0800

    upload

---
+++ 2024_06_17/lesson1.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import Misc
+
+class Example(ttk.Frame):
+    def __init__(self,master:Misc,**kwargs):
+        super().__init__(master=master,**kwargs)
+        master.title('Lines')
+        self.configure({'borderwidth':2,'relief':'groove'})
+        #self.config({'borderwidth':2,'relief':'groove'})
+        #self['borderwidth'] = 2
+        #self['relief'] = 'groove'
+        self.pack(expand=True,fill='both')
+
+def mian():
+    window = tk.Tk()
+    
+    Example(window)
+    window.geometry("400x250")
+    window.mainloop()
+
+if __name__ == "__main__":
+    mian()
