8ff1153489c4a692f59427a43a8d9a5163199a13
DLNY971237 <danny971237@gmail.com>
2024-07-15 11:45:11 +0800

    modify

--- 2024_07_11/test.sql
+++ 2024_07_11/test.sql
@@ -1,31 +1,21 @@
 SELECT *
-FROM youbike
+FROM youbike;
 
 
-SELECT *
-FROM youbike
-WHERE sarea IN ('大安區','士林區')
-
-SELECT MAX(updatetime),sna
-FROM youbike
-GROUP BY sna
-
-------儲存最新資料進SQL--------------
-SELECT *
-FROM youbike
-WHERE (updatetime, sna) IN (
-	SELECT MAX(updatetime),sna
-	FROM youbike
-	GROUP BY sna
-)
-----------------------------------
+SELECT DISTINCT sarea
+FROM youbike;
 
 SELECT sna as 站點, total as 總車輛數, rent_bikes as 可借, return_bikes as 可還, mday as 時間, act as 狀態
 FROM youbike
-WHERE (updatetime, sna) IN (
-	SELECT MAX(updatetime),sna
-	FROM youbike
-	WHERE sarea = '大同區'
-	GROUP BY sna
+WHERE (updatetime,sna) IN (
+	SELECT MAX(updatetime), sna
+FROM youbike
+WHERE sarea = '大同區'
+GROUP BY sna
 )
 
+
+SELECT MAX(updatetime), sna
+FROM youbike
+WHERE sarea = '士林區'
+GROUP BY sna
