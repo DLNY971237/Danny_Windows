d6818feb642716ee096570b5491540c8439cd5c0
DLNY971237 <danny971237@gmail.com>
2024-07-22 16:42:19 +0800

    modify

--- 2024_07_22_callback/callback5.py
+++ 2024_07_22_callback/callback5.py
@@ -1,17 +1,32 @@
-from dash import Dash, dcc, html, Input, Output, callback
+from dash import Dash, dcc, html, Input, Output, callback, dash_table
 import data
+import pandas as pd
 
 app = Dash(__name__)
 
 areas = [tup[0] for tup in data.get_areas()]
+print(areas[0])
 
 app.layout = html.Div([
     dcc.Dropdown(
-       options = areas,
-       value = areas[0],
-       id='areas' 
-    )
+       options=areas,
+       value=areas[0],  # 設置初始值為第一個區域的值
+       id='areas'
+    ),
+    html.Hr(),
+    dash_table.DataTable(id='sites_table')
 ])
+
+@callback(
+    Output('sites_table', 'data'),
+    Input('areas', 'value')
+)
+def selected_area(area_value):
+    # 根據選定的區域獲取數據
+    content = data.get_snaOfArea(area=area_value)
+    df = pd.DataFrame(content)
+    df.columns = ['站點名稱', '總數', '可借', '可還', '日期', '狀態']
+    return df.to_dict('records') 
 
 if __name__ == '__main__':
     app.run(debug=True)
