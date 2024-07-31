537ee0f223cf65fae422ce4fe37033bee235c66c
DLNY971237 <danny971237@gmail.com>
2024-07-31 15:07:59 +0800

    modify

--- test_0730.sql
+++ test_0730.sql
@@ -3,16 +3,16 @@ CREATE TABLE IF NOT EXISTS 使用者(
 	姓名 VARCHAR(20),
 	性別 VARCHAR(20),
 	聯絡電話 VARCHAR(20),
-	電子郵件 VARCHAR(40),
+	電子郵件 VARCHAR(40) UNIQUE,
 	isGetEmail Bool,
 	出生年月日 VARCHAR(20),
 	自我介紹 VARCHAR(200),
 	密碼 VARCHAR(100),
-	連線密碼 VARCHAR(20)
+	連線密碼 VARCHAR(100)
 );
 
-INSERT INTO 使用者(姓名,電子郵件,密碼) 
-VALUES('Danny','da@gmail.com','12345')
+INSERT INTO 使用者(姓名,性別,聯絡電話,電子郵件,isGetEmail,出生年月日,自我介紹,密碼,連線密碼) 
+VALUES('Danny','男','0912-123-123','da@gmail.com',True,'1990-03-05','我是Danny','12345','678910')
 
 SELECT *
 FROM 使用者
